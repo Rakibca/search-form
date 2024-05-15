0c9318fee8feb56178eb96d48e0b3b30c77c0d54
Rakibul Islam <rakib.ca@gmail.com>
2024-05-15 10:52:20 -0600

    search form added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 function App() {
-  return <form>"Hello"</form>;
+  return (
+    <form>
+      <label htmlFor="search-input">Search:</label>
+      <input id="search-input" />
+      <button aria-label="Submit" className="submit-btn">
+        <img
+          alt=""
+          src="https://sandpack-bundler.vercel.app/img/arrow-right.svg"
+        />
+      </button>
+    </form>
+  );
 }
 
 export default App;
